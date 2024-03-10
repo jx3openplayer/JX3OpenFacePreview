8fbfccbf77e41d3e88bb37df4c95027eee102a6d
JX3OpenPlayer <jx3openplayer@outlook.com>
2024-03-10 13:23:05 +0800

    尝试将数据库改为永久

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,6 +3,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+if (navigator.storage && navigator.storage.persist)
+    navigator.storage.persist().then(function (persistent) {
+        if (persistent)
+            console.log("Storage will not be cleared except by explicit user action");
+        else
+            console.log("Storage may be cleared by the UA under storage pressure.");
+    });
+
 const app = createApp(App)
 
 app.use(router)
